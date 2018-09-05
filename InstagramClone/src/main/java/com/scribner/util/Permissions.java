0dfc0b2a56d7b6b5e8b19a277fe20dbc21ccf6de
wescrib <scribner.william@gmail.com>
2018-09-04 20:02:35 -0400

    bottom tab started..still blank at this point

---
+++ InstagramClone/src/main/java/com/scribner/util/Permissions.java
@@ -0,0 +1,13 @@
+package com.scribner.util;
+
+import android.Manifest;
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+}
