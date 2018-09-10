ec1878e13595720c0c3a3ad4405a187bf3f07575
wescrib <scribner.william@gmail.com>
2018-09-10 17:06:44 -0400

    added snippet top gallery toolbar

--- InstagramClone/src/main/java/com/scribner/instagram/Share/GalleryFragment.java
+++ InstagramClone/src/main/java/com/scribner/instagram/Share/GalleryFragment.java
@@ -7,17 +7,49 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.scribner.instagram.R;
 
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
 
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mPrograssbar;
+    private Spinner directorySpinner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        galleryImage = view.findViewById(R.id.galleryImageView);
+        gridView = view.findViewById(R.id.gridView);
+        directorySpinner = view.findViewById(R.id.spinnerDirectory);
+        mPrograssbar = view.findViewById(R.id.progressBar);
+        mPrograssbar.setVisibility(View.GONE);
         Log.d(TAG, "onCreateView: started");
+
+        ImageView shareClose = view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the gallery fragment");
+                getActivity().finish();
+            }
+        });
+
+        TextView nextScreen = view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to final share screen");
+            }
+        });
 
         return view;
     }
